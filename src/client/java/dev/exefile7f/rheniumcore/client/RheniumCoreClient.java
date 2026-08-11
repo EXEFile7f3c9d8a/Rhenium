fb58a05e65f558c91ddf8c7e0fd020df1be77084
EXEFile_7f3c9d8a <exefile.7f3c9d8a@gmail.com>
2026-08-11 19:00:28 -0400

    Update
    i was trying to make the modmenu interface and as u see i not done but why not commit?

--- src/client/java/dev/exefile7f/rheniumcore/client/RheniumCoreClient.java
+++ src/client/java/dev/exefile7f/rheniumcore/client/RheniumCoreClient.java
@@ -1,9 +1,12 @@
 package dev.exefile7f.rheniumcore.client;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 
-public class RheniumCoreClient implements ClientModInitializer {
+public class RheniumCoreClient implements ClientModInitializer{
 	@Override
-	public void onInitializeClient() {
-	}
+	public void onInitializeClient(){
+        AutoConfig.register(ModSettings.class, GsonConfigSerializer::new);
+    }
 }
