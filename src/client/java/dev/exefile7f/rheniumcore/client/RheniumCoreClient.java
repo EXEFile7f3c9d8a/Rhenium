68631da5bff070279a77062d0669ff3cb8b36cf3
EXEFile_7f3c9d8a <exefile.7f3c9d8a@gmail.com>
2026-08-14 13:24:30 -0400

    Initialize Config output
    client now output the config path of this mod

--- src/client/java/dev/exefile7f/rheniumcore/client/RheniumCoreClient.java
+++ src/client/java/dev/exefile7f/rheniumcore/client/RheniumCoreClient.java
@@ -1,12 +1,14 @@
 package dev.exefile7f.rheniumcore.client;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import dev.exefile7f.rheniumcore.statics.StaticResource;
 import net.fabricmc.api.ClientModInitializer;
+
+import static dev.exefile7f.rheniumcore.RheniumCore.LOGGER;
+import static dev.exefile7f.rheniumcore.RheniumCore.MOD_ID;
 
 public class RheniumCoreClient implements ClientModInitializer{
 	@Override
 	public void onInitializeClient(){
-        AutoConfig.register(ModSettings.class, GsonConfigSerializer::new);
+        LOGGER.debug("Config Path of {}: {}", MOD_ID, StaticResource.CONFIG_PATH);
     }
 }
