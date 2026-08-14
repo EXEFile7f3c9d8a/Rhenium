fa8f9f0edebdabb7db1464ff663196758ef1e110
EXEFile_7f3c9d8a <exefile.7f3c9d8a@gmail.com>
2026-08-14 11:56:16 -0400

    First config screen
    i tried a little and thats it i got a config screen with a single toggle switch, no saving but least i can make it run

--- src/client/java/dev/exefile7f/rheniumcore/client/ModMenuIntegration.java
+++ src/client/java/dev/exefile7f/rheniumcore/client/ModMenuIntegration.java
@@ -2,15 +2,33 @@ package dev.exefile7f.rheniumcore.client;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.text.Text;
+
+import static dev.exefile7f.rheniumcore.RheniumCore.LOGGER;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi{
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModSettings.class, parent).get();
+        return parent -> {
+            ConfigBuilder builder = ConfigBuilder.create()
+                    .setParentScreen(parent)
+                    .setTitle(Text.of("Title"))
+                    .solidBackground();
+            ConfigEntryBuilder toggle = builder.entryBuilder();
+            ConfigCategory general = builder.getOrCreateCategory(Text.of("catagory"));
+            general.addEntry(toggle.startBooleanToggle(Text.of("switch"), false)
+                    .setDefaultValue(false)
+                    .setSaveConsumer((value) ->{
+                        LOGGER.info(String.valueOf(value));
+                    }).build());
+            return builder.build();
+        };
     }
 }
